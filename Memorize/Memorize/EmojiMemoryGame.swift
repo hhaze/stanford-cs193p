2c253ae1f59fb0ed65df24863945953b9f892a72
jhgu <jhgu.dev@gmail.com>
2020-07-04 21:56:33 +0900

    Reactive UI
    - Lecture 3: Reactive UI + Protocols + Layout

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,8 +8,8 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["👻", "🎃", "🕷"]
@@ -17,7 +17,7 @@ class EmojiMemoryGame {
             return emojis[pairIndex]
         }
     }
-    
+        
     // MARK: - Access to the Model
     
     var cards: Array<MemoryGame<String>.Card> {
